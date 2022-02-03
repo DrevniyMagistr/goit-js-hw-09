6731a3d0723d55cae009618be5d0d5a154ae8f59
DrevniyMagistr <sasha.klishch@gmail.com>
2022-02-03 19:48:08 +0200

    added switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
 
+let timerId = null;
+let off = refs.btnStop.setAttribute('disabled', true);
+
+const onStartButton = () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  toggle();
+};
+
+const onStopButton = () => {
+  clearInterval(timerId);
+
+  toggle();
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.btnStart.addEventListener('click', onStartButton);
+refs.btnStop.addEventListener('click', onStopButton);
+
+function toggle() {
+  if ((off = !off)) {
+    refs.btnStop.removeAttribute('disabled');
+    refs.btnStart.setAttribute('disabled', true);
+  } else {
+    refs.btnStop.setAttribute('disabled', true);
+    refs.btnStart.removeAttribute('disabled');
+  }
+}
